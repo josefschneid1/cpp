6b083611977c1b94e47c9c5f2a2ef98ef9ae07d5
Josef Schneid <josefschneid@josefs-mbp-2>
2018-07-21 17:58:27 +0200

    Kapitel6

--- Kapitel6/Kapitel6/main.cpp
+++ Kapitel6/Kapitel6/main.cpp
@@ -1,15 +1,113 @@
-//
-//  main.cpp
-//  Kapitel6
-//
-//  Created by Josef Schneid on 20.07.18.
-//  Copyright © 2018 Josef Schneid. All rights reserved.
-//
-
 #include <iostream>
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <cmath>
+#include <algorithm>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+/*
+    Term->Zahl|Term Operator Term|(Term)
+    Operator->+|-|/|*
+*/
+
+struct Token{
+    char kind;
+    double value;
+};
+
+bool isValidOperator(char c)
+{
+    if(c=='+' || c=='-' || c=='*' || c=='/')
+        return true;
+    else
+        return false;
+}
+
+bool isValidBracket(char c)
+{
+    if(c==')'||c=='(')
+        return true;
+    else
+        return false;
+}
+
+vector<Token> inputToTokenStream(istream& input)
+{
+    vector<Token> tokenStream;
+    string eingabe;
+    getline(input, eingabe);
+    int stack = 0;
+    while(!eingabe.empty())
+    {
+        if(isnumber(eingabe.at(0)))//Zahl
+        {
+            int index = 0;
+            while(index < eingabe.size() && isnumber(eingabe.at(index)))
+                index++;
+            tokenStream.push_back(Token{'n',stod(eingabe.substr(0,index))});
+            eingabe = eingabe.substr(index);
+            
+        }else if(tokenStream.size()>0
+                 && (tokenStream[tokenStream.size()-1].kind==')' || tokenStream[tokenStream.size()-1].kind=='n')
+                 &&isValidOperator(eingabe.at(0)))//Operator
+        {
+            tokenStream.push_back(Token{eingabe.at(0)});
+            eingabe = eingabe.substr(1);
+            
+        }else if(isValidBracket(eingabe.at(0)))//Klammer
+        {
+            if(eingabe.at(0)== '(')
+                stack++;
+            else{
+                if(stack < 1 || isValidOperator(tokenStream[tokenStream.size()-1].kind))
+                {
+                    throw invalid_argument("MathError");
+                }else
+                    stack--;
+                
+            }
+            tokenStream.push_back(Token{eingabe.at(0)});
+            eingabe = eingabe.substr(1);
+            
+        }else if(eingabe.at(0)==' ')//Leerzeichen
+        {
+            eingabe = eingabe.substr(1);
+        }else{//Nicht gültiges Zeichen
+            throw invalid_argument("MathError");
+        }
+        
+    }
+    if(stack>0 || isValidOperator(tokenStream[tokenStream.size()-1].kind))
+        throw invalid_argument("MathError");
+    
+    return tokenStream;
+}
+
+void printTokenStream(ostream& ausgabe, vector<Token> stream)
+{
+    for(Token t : stream)
+    {
+        if(t.kind=='n')
+        {
+            ausgabe << t.value << ", ";
+        }else{
+            ausgabe << t.kind << ", ";
+        }
+    }
+    ausgabe << '\n';
+}
+
+int main()
+{
+    try {
+        vector<Token>tokenStream = inputToTokenStream(cin);
+        printTokenStream(cout, tokenStream);
+    } catch (invalid_argument e) {
+        cout << e.what() << '\n';
+    }
+    
     return 0;
 }
+
+
